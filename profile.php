0153f40a0091f0b375ba83e76989ecaa69651d25
jmkc <jacquelinemiriam@gmail.com>
2016-03-27 14:30:16 -0400

    Profile
    Update profile to go to view bookings, and profile update form

--- profile.php
+++ profile.php
@@ -60,28 +60,10 @@ if(isset($_SESSION['member_id'])){
 ?>
 Welcome  <?php echo $myrow['FName']; ?>, <a href="index.php?logout=1">Log Out</a><br/>
 <!-- dynamic content will be here -->
-<form name='editProfile' id='editProfile' action='profile.php' method='post'>
-    <table border='0'>
-        <!--<tr>
-            <td>Username</td>
-            <td><input type='text' name='username' id='username' disabled  value="<?php echo $myrow['username']; ?>"  /></td>
-        </tr>-->
-        <tr>
-            <td>Password</td>
-             <td><input type='text' name='password' id='password'  value="<?php echo $myrow['password']; ?>" /></td>
-        </tr>
-        <tr>
-            <td>Email</td>
-            <td><input type='text' name='email' id='email'  value="<?php echo $myrow['email']; ?>" /></td>
-        </tr>
-        <tr>
-            <td></td>
-            <td>
-                <input type='submit' name='updateBtn' id='updateBtn' value='Update' /> 
-            </td>
-        </tr>
-    </table>
-</form>
+<!-- Update profile-->
+<input type='update' name='goToUpdate' id='goToUpdate' value='Update Profile' \>
+<!-- View Booking profile-->
+<input type='viewBookings' name='viewBookings' id='viewBookings' value='View Bookings' \>
 </body>
 
 </html>
