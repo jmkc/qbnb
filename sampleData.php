c5c0f0f58959c3caeb57fd27a83d6513cd4ed642
jmkc <jacquelinemiriam@gmail.com>
2016-03-27 14:04:05 -0400

    Add data
    Data being added

--- sampleData.php
+++ sampleData.php
@@ -143,6 +143,57 @@ mysqli_query($cxn, "CREATE TABLE IF NOT EXISTS `qbnb`.`Feature` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)");
 
+
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Member` (`member_id`, `email`, `password`, `FName`, `LName`, `year`, `faculty`, `degree`, `is_admin`, `is_deleted`) VALUES (1, 'jshimz@hotmail.com', 'itsjustshim', 'Justin', 'Shimkovitz', 2016, 'Law', 'Criminal Law', 1, DEFAULT)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Member` (`member_id`, `email`, `password`, `FName`, `LName`, `year`, `faculty`, `degree`, `is_admin`, `is_deleted`) VALUES (2, 'coolboyjulez@queensu.ca', 'jwill', 'Julian', 'Wilson', 2016, 'Commerce', 'Finance', 1, 0)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Member` (`member_id`, `email`, `password`, `FName`, `LName`, `year`, `faculty`, `degree`, `is_admin`, `is_deleted`) VALUES (3, 'jcisgod@gmail.com', 'jacjacjac', 'Jacqueline', 'Craig', 2016, 'Engineering', 'Applied Math', 1, 0)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Member` (`member_id`, `email`, `password`, `FName`, `LName`, `year`, `faculty`, `degree`, `is_admin`, `is_deleted`) VALUES (4, 'fakeemail@hotmail.com', 'animals', 'John', 'Stewart', 2018, 'Science', 'Biology', 0, 0)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Member` (`member_id`, `email`, `password`, `FName`, `LName`, `year`, `faculty`, `degree`, `is_admin`, `is_deleted`) VALUES (5, 'potus@gmail.com', 'password', 'Barack', 'Obama', 2019, 'Arts', 'English', 0, 0)");
+
+mysqli_query($cxn,"INSERT INTO `qbnb`.`District` (`district_id`, `district_name`, `Street_1`, `Street_2`) VALUES (1, 'Uptown', 'Bathurst', 'Steeles')");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`District` (`district_id`, `district_name`, `Street_1`, `Street_2`) VALUES (2, 'Waterfront', 'Front', 'Spadina')");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`District` (`district_id`, `district_name`, `Street_1`, `Street_2`) VALUES (3, 'Rosedale', 'St. Clair', 'Oak')");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`District` (`district_id`, `district_name`, `Street_1`, `Street_2`) VALUES (4, 'Chinatown', 'Spadina', 'Queen')");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`District` (`district_id`, `district_name`, `Street_1`, `Street_2`) VALUES (5, 'Midtown', 'Bathurst', 'Eglinton')");
+
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Property` (`property_id`, `address`, `number_of_rooms`, `room_type`, `price`, `is_deleted`, `owner_id`, `district_id`) VALUES (1, '123 Maple Street', 4, 'House', 600, 0, 1, 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Property` (`property_id`, `address`, `number_of_rooms`, `room_type`, `price`, `is_deleted`, `owner_id`, `district_id`) VALUES (2, '136 Oak Drive', 3, 'House', 200, 0, 1, 4)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Property` (`property_id`, `address`, `number_of_rooms`, `room_type`, `price`, `is_deleted`, `owner_id`, `district_id`) VALUES (3, '1 Front Street', 1, 'Apartment', 1000, 0, 2, 2)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Property` (`property_id`, `address`, `number_of_rooms`, `room_type`, `price`, `is_deleted`, `owner_id`, `district_id`) VALUES (4, '12 Wall Avenue', 2, 'Condo', 750, 0, 5, 2)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Property` (`property_id`, `address`, `number_of_rooms`, `room_type`, `price`, `is_deleted`, `owner_id`, `district_id`) VALUES (5, '10 Martin Lane', 2, 'Apartment', 500, 0, 3, 4)");
+
+
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Booking` (`booking_id`, `status`, `start_date`, `is_deleted`, `booking_member_id`, `property_id`) VALUES (1, 'Confirmed', '2015-05-06', 0, 2, 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Booking` (`booking_id`, `status`, `start_date`, `is_deleted`, `booking_member_id`, `property_id`) VALUES (2, 'Deleted', '2015-05-06', 0, 2, 2)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Booking` (`booking_id`, `status`, `start_date`, `is_deleted`, `booking_member_id`, `property_id`) VALUES (3, 'Denied', '2015-05-08', 0, 3, 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Booking` (`booking_id`, `status`, `start_date`, `is_deleted`, `booking_member_id`, `property_id`) VALUES (4, 'Unconfirmed', '2015-05-09', 0, 4, 2)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Booking` (`booking_id`, `status`, `start_date`, `is_deleted`, `booking_member_id`, `property_id`) VALUES (5, 'Confirmed', '2015-08-04', 0, 1, 3)");
+
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Phone_Number` (`phone_number`, `member_id`) VALUES ('905-881-4432', 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Phone_Number` (`phone_number`, `member_id`) VALUES ('416-543-1234', 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Phone_Number` (`phone_number`, `member_id`) VALUES ('416-543-1233', 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Phone_Number` (`phone_number`, `member_id`) VALUES ('789-087-5432', 2)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Phone_Number` (`phone_number`, `member_id`) VALUES ('818-987-4324', 3)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Phone_Number` (`phone_number`, `member_id`) VALUES ('123-456-7867', 3)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Phone_Number` (`phone_number`, `member_id`) VALUES ('453-235-4433', 4)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Phone_Number` (`phone_number`, `member_id`) VALUES ('323-655-6666', 5)");
+
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Points_Of_Interest` (`points_of_interest`, `district_id`) VALUES ('C.N. Tower', 2)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Points_Of_Interest` (`points_of_interest`, `district_id`) VALUES ('Beach', 2)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Points_Of_Interest` (`points_of_interest`, `district_id`) VALUES ('R.O.M.', 3)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Points_Of_Interest` (`points_of_interest`, `district_id`) VALUES ('Concert Hall', 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Points_Of_Interest` (`points_of_interest`, `district_id`) VALUES ('Bell Movie Theatre', 4)");
+
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Feature` (`feature_id`, `feature`, `property_id`) VALUES (1, '2 Bathrooms', 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Feature` (`feature_id`, `feature`, `property_id`) VALUES (2, 'Big Backyard', 3)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Feature` (`feature_id`, `feature`, `property_id`) VALUES (3, 'Pool', 4)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Feature` (`feature_id`, `feature`, `property_id`) VALUES (4, 'Pool', 2)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Feature` (`feature_id`, `feature`, `property_id`) VALUES (5, 'Queen Beds', 1)");
+
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Comment` (`comment_id`, `text`, `Rating`, `Date`, `is_deleted`, `commenting_member_id`, `property_id`) VALUES (1, 'Great place! Slightly dirty, but nothing unmanageable', 4, '2015-05-05', 0, 5, 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Comment` (`comment_id`, `text`, `Rating`, `Date`, `is_deleted`, `commenting_member_id`, `property_id`) VALUES (2, 'Thanks for your feedback! I\'ll make sure to get that fixed', NULL, '2015-05-06', 0, 1, 1)");
+mysqli_query($cxn,"INSERT INTO `qbnb`.`Comment` (`comment_id`, `text`, `Rating`, `Date`, `is_deleted`, `commenting_member_id`, `property_id`) VALUES (3, 'Terrible. Really terrifying area of town and nothing near it worth seeing. Would not recommend.', 1, '2016-01-03', 0, 4, 3)");
+
  mysqli_close($cxn); 
 
 ?>
