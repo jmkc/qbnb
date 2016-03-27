157096447f4c16c7cc95466ca034d5a928639053
jmkc <jacquelinemiriam@gmail.com>
2016-03-27 17:43:17 -0400

    Create BookingTools
    See a booking

---
+++ Classes/Bookingtools.php
@@ -0,0 +1,22 @@
+<?php
+//UserTools.class.php
+
+require_once 'Member.php';
+require_once 'Database.php';
+
+class Bookingtools
+{
+	
+	//get a booking
+	//returns a Booking object. Takes the booking id as an input
+	public function get($booking_id)
+	{
+		$db = new Database();
+		$result = $db->select('Booking', "booking_id = $booking_id");
+
+		return new Booking($result);
+	}
+	
+}
+
+?>
