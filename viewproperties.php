19fbaa06d77a143f453d584e0b299585287b5798
jmkc <jacquelinemiriam@gmail.com>
2016-03-27 18:34:06 -0400

    view properties
    View all properties that are in the system

--- viewproperties.php
+++ viewproperties.php
@@ -1,13 +1,26 @@
-<?php
+<?php 
+ 
 require_once 'global.inc.php';
-$data = mysql_query("SELECT * FROM Property");
-if (mysql_num_rows($data) > 0){
-    for($i = 0; $i < mysql_num_rows($data); $i++){
-        $row = mysql_fetch_row($data);
-        echo "Address: ", $row[1], " Number of Rooms: ", $row[2], " Room Type: ", $row[3], " Price: ", $row[4],"\r";
-    }
+ 
+//check to see if they're logged in
+if(!isset($_SESSION['logged_in'])) {
+    header("Location: index.php");
 }
-else{
-    echo "No results";
-}
+$allProperties = mysql_query("SELECT * FROM Property");
 ?>
+<html>
+<head>
+    <title>Qbnb | View All Properties</title>
+</head>
+<body>
+    <?php echo $message; ?>
+ 	<?php
+ 	while($property = mysql_fetch_assoc($allProperties))
+ 	{
+ 		extract($property);
+ 		echo "<br />$property_id, Address:$address, $number_of_rooms, $room_type, $price <br />"
+ 	}
+
+ 	?>
+</body>
+</html>
