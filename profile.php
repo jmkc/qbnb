34b2a15285def130a5116341a45ad682683a9df3
jmkc <jacquelinemiriam@gmail.com>
2016-03-27 13:04:21 -0400

    User Update
    Update the user information if possible

--- profile.php
+++ profile.php
@@ -9,6 +9,25 @@
   //Create a user session or resume an existing one
  session_start();
  ?>
+<?php
+ 
+ if(isset($_POST['updateBtn']) && isset($_SESSION['id'])){
+  // include database connection
+    include_once 'config/connection.php'; 
+    
+    $query = "UPDATE user SET password=?,email=? WHERE id=?";
+ 
+    $stmt = $con->prepare($query);
+    $stmt->bind_param('sss', $_POST['password'], $_POST['email'], $_SESSION['id']);
+    // Execute the query
+        if($stmt->execute()){
+            echo "Record was updated. <br/>";
+        }else{
+            echo 'Unable to update record. Please try again. <br/>';
+        }
+ }
+ 
+ ?>
  <?php
 if(isset($_SESSION['id'])){
    // include database connection
