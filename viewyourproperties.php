295ef7bacd57b94da475fc4e6093f80ea465a3f2
jmkc <jacquelinemiriam@gmail.com>
2016-03-27 21:14:08 -0400

    Added Add new to view own properties
    Added add new property to view your own property

--- viewyourproperties.php
+++ viewyourproperties.php
@@ -6,30 +6,40 @@ require_once 'global.inc.php';
 if(!isset($_SESSION['logged_in'])) {
     header("Location: index.php");
 }
-
-if(isset($_POST['back'])) { 
-    header("Location: profile.php");
-} 
-
 $member = unserialize($_SESSION['member_id']);
+
 $allProperties = mysql_query("SELECT * FROM Property WHERE owner_id = $member->member_id");
+
+if(isset($_POST['cancel'])) { 
+    header("Location: profile.php");
+}
+
+if(isset($_POST['viewproperty'])) { 
+    $_SESSION['property_id'] = $_POST['viewproperty'];
+    header("Location: propertyInfo.php");
+}
+
+if(isset($_POST['addProperty'])) { 
+    header("Location: addProperty.php");
+}
+
 ?>
 <html>
 <head>
-    <title>Qbnb | View Your Properties</title>
+    <title>Qbnb | View All Properties</title>
 </head>
 <body>
  	<?php
+ 	echo "<form action='viewproperties.php' method='post'>";
  	while($property = mysql_fetch_assoc($allProperties))
  	{
  		extract($property);
  		echo "<br />Address: $address - Number of Rooms: $number_of_rooms - Room Type: $room_type - Price: $price <br />";
+ 		echo "<input type='submit' value=$property_id name='viewproperty' />";
  	}
+ 	echo "<input type='submit' value='Add Property' name='addProperty' />"
+ 	echo "<input type='submit' value='Cancel' name='cancel' /></form>";
 
  	?>
-<form name='options' id='options' action='profile.php' method='post'>
-<input type='submit' name='back' id='back' value='Back' /> 
-</form>
 </body>
-
 </html>
