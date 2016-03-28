f51dbaa9c5c6ed546412a648a1c1ede9b6fabc7b
JustinShimkovitz <justinshimkovitz@hotmail.com>
2016-03-28 17:15:48 -0400

    See POIs
    Can see POIs

--- viewyourbookings.php
+++ viewyourbookings.php
@@ -18,16 +18,24 @@ if(isset($_POST['cancel'])) {
 ?>
 <html>
 <head>
-    <title>Qbnb | View All Properties</title>
+    <title>Qbnb | View Your Bookings</title>
 </head>
 <body>
+    <h1>View Your Bookings</h1>
  	<?php
  	echo "<form action='viewyourbookings.php' method='post'>";
  	while($booking = mysql_fetch_assoc($yourBookings))
  	{
  		extract($booking);
- 		echo "<br />Address: $address - Number of Rooms: $number_of_rooms - Room Type: $room_type - Price: $price - Owner: $FName $LName - Move In Date: $start_date <br />";
- 		//echo "<input type='submit' value=$property_id name='viewproperty' />";
+ 		echo "<br />Address: $address <br /> Number of Rooms: $number_of_rooms - Room Type: $room_type - Price: $price - Owner: $FName $LName - Move In Date: $start_date <br />";
+ 		$sql = "select * from Points_Of_Interest where district_id = $district_id";
+        echo "Points of Interest:";
+        $POIs = mysql_query($sql);
+        while ($POI = mysql_fetch_assoc($POIs)){
+            extract($POI);
+            echo "$points_of_interest<br \>";
+        }
+         //echo "<input type='submit' value=$property_id name='viewproperty' />";
  	}
  	//echo "<input type='submit' value='Add Property' name='addProperty' />";
  	echo "<input type='submit' value='Cancel' name='cancel' /></form>";
