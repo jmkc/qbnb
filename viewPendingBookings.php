dbdec128b2cf4eb80613198c2878fb38b028ef82
jmkc <jacquelinemiriam@gmail.com>
2016-03-28 00:06:32 -0400

    View pending bookings trial
    View the trial pending bookings

--- viewPendingBookings.php
+++ viewPendingBookings.php
@@ -6,36 +6,58 @@ require_once 'global.inc.php';
 if(!isset($_SESSION['logged_in'])) {
     header("Location: index.php");
 }
-$member = unserialize($_SESSION['member_id']);
+ 
+//get the user object from the session
+$data['property_id'] = $_SESSION['property_id'];
+$data['member_id'] = unserialize($_SESSION['member_id']);
 
-$allProperties = mysql_query("SELECT * FROM Property natural join Booking WHERE status = 'Unconfirmed'");
+$allProperties = mysql_query("SELECT * 
+    FROM Property natural join Booking 
+    WHERE status = 'Unconfirmed'
+    and owner_id = $member_id");
 
-if(isset($_POST['cancel'])) { 
-    header("Location: profile.php");
+
+//check to see that the form has been submitted
+if(isset($_POST['submit-updateConfirm'])) { 
+ 
+    //retrieve the $_POST variables
+
+    $data['status'] = 'Confirmed';
+
+    $updateBooking = new Booking($data);
+    $updateProperty->save();
+ 
+    $message = "Settings Saved<br/>";
 }
 
-if(isset($_POST['status'])) { 
-    $_SESSION['property_id'] = $_POST['viewproperty'];
+if(isset($_POST['submit-updateProperty'])) { 
+ 
+    //retrieve the $_POST variables
     header("Location: propertyInfo.php");
 }
-
+ 
+//If the form wasn't submitted, or didn't validate
+//then we show the registration form again
 ?>
+ 
 <html>
 <head>
-    <title>Qbnb | View Pending Bookings</title>
+    <title>Qbnb | Update Property</title>
 </head>
 <body>
- 	<?php
- 	echo "<form action='viewPendingBookings.php' method='post'>";
- 	while($booking = mysql_fetch_assoc($allProperties))
- 	{
- 		extract($property);
- 		echo "<br />Address: $address - Date: $start_date - Room Type: $room_type - Price: $price <br />";
- 		echo "<input type='submit' value=$status name='status' />";
- 	}
- 	
- 	echo "<input type='submit' value='Cancel' name='cancel' /></form>";
-
- 	?>
+    <?php
+    echo "<form action='viewPendingBookings.php' method='post'>";
+    while($booking = mysql_fetch_assoc($allProperties))
+    {
+        extract($booking);
+        echo "<br />Address: $address - Date: $start_date - Status: $status <br />";
+        echo "<input type='submit' value='Confirm' name='submit-updateConfirm' />";
+        echo "<input type='submit' value='Deny' name='submit-updateDeny' />";
+    }
+    echo "</form>";
+    ?>
+    <form action="profile.php" method="post">
+    <input type='submit' value='Cancel' name='cancel' />
+    </form>
 </body>
 </html>
