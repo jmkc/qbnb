ac78268c5f4b8a0b00f325ad9c8a862b24ca4058
JustinShimkovitz <justinshimkovitz@hotmail.com>
2016-03-28 17:15:27 -0400

    Delete capabilities for property
    Can delete property

--- propertyInfo.php
+++ propertyInfo.php
@@ -12,6 +12,7 @@ if(!isset($_SESSION['logged_in'])) {
 //get the user object from the session
 $property_id = $_SESSION['property_id'];
 $property_info = mysql_query("SELECT * FROM Property WHERE property_id = $property_id");
+$feature_info = mysql_query("SELECT * FROM Feature WHERE property_id = $property_id");
 $property_bookings = mysql_query("SELECT * FROM Booking WHERE property_id = $property_id and status = 'Confirmed'");
 $property_owner = mysql_query("SELECT * FROM Member WHERE member_id = (SELECT owner_id FROM Property WHERE property_id = $property_id)");
 $property_comments = mysql_query("SELECT * FROM Comment inner join Member on commenting_member_id = member_id WHERE property_id = $property_id");
@@ -28,6 +29,16 @@ if(isset($_POST['bookProp'])) {
 if(isset($_POST['comment'])) { 
     header("Location: makeComment.php");
 }
+if(isset($_POST['delete'])) { 
+    $sql = "UPDATE Property SET is_deleted = 1 where property_id = $property_id";
+    $sql_comment = "UPDATE Comment SET is_deleted = 1 where property_id = $property_id";
+    $sql_booking = "UPDATE Booking SET is_deleted = 1 where property_id = $property_id";
+    mysql_query($sql) or die(mysql_error());
+    mysql_query($sql_comment) or die(mysql_error());
+    mysql_query($sql_booking) or die(mysql_error());
+    header("Location: viewproperties.php");
+}
+
 ?>
  
 <html>
@@ -45,23 +56,28 @@ if(isset($_POST['comment'])) {
     extract($owner);
     $owner_id = $member_id;
     echo "<br/> Owned By: $FName $LName<br />";
-    
+    echo "<h2> Features</h2>";
+    if(mysql_num_rows($feature_info) != 0){
+        while($features = mysql_fetch_assoc($feature_info)){
+            extract($features);
+            echo "<br/> $feature<br />";
+        }
+    }
     if(mysql_num_rows($property_bookings) == 0){
         echo "<br/> No current bookings<br />";
     }
     else{
-    echo "<br/> Unavailable during the weeks starting on these dates:<br />";
-    while($bookings = mysql_fetch_assoc($property_bookings)){
-        extract($bookings);
-        echo "<br/> $start_date<br />";
+        echo "<h2> Unavailable during the weeks starting on these dates:</h2>";
+        while($bookings = mysql_fetch_assoc($property_bookings)){
+            extract($bookings);
+            echo "<br/> $start_date<br />";
+        }
     }
-    }
-  
      if(mysql_num_rows($property_comments) == 0){
-        echo "<br/> No current comments<br />";
+        echo "<h2> No current comments</h2>";
     }
     else{
-    echo "<br/> Comments:<br />";
+    echo "<h2> Comments:</h2>";
     while($comments = mysql_fetch_assoc($property_comments)){
         extract($comments);
         echo "<br/> On $Date, $FName $LName said: $text<br />";
@@ -73,6 +89,8 @@ if(isset($_POST['comment'])) {
         echo"<form name='options' id='options' action='updateProperty.php' method='post'>
 <input type='submit' name='updateProp' id='updateProp' value='Update Property' /> 
 </form>";
+        echo "<form name='options' id='options' action='viewproperties.php' method='post'>";
+        echo "<input type='submit' name='delete' id='delete' value='Delete Property' /></form>"; 
     }
     else{
         echo"<form name='options' id='options' action='Book.php' method='post'>
@@ -84,8 +102,7 @@ if(isset($_POST['comment'])) {
 <form name='options' id='options' action='makeComment.php' method='post'>
 <input type='submit' name='comment' id='comment' value='Comment' /> 
 </form>
- 
- 
+
 
 <form name='options' id='options' action='viewproperties.php' method='post'>
 <input type='submit' name='backProperties' id='backProperties' value='Back' /> 
