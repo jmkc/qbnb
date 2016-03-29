0e56d54c07b60e419434eaa9ab5fc7afce49b3c8
JustinShimkovitz <justinshimkovitz@hotmail.com>
2016-03-28 20:06:38 -0400

    Validate booking date
    Validate booking date

--- Book.php
+++ Book.php
@@ -7,36 +7,38 @@ require_once 'global.inc.php';
 
 $status = "";
 $start_date = "";
-
+$message = "";
 //check to see that the form has been submitted
 if(isset($_POST['submit-form'])) { 
     
     //retrieve the $_POST variables
     $start_date = $_POST['start_date'];
-    // if ($start_date = )
-    //initialize variables for form validation
-    $bookingTools = new Bookingtools();
-    //prep the data for saving in a new user object
-    $data['start_date'] = $start_date;
-    $data['property_id'] = $_SESSION['property_id'];
-    $member = unserialize($_SESSION['member_id']);
-    $data['booking_member_id'] = $member->member_id;
-    //create the new booking object
-    $newBooking = new Booking($data);
+    if (preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$start_date)){
+        //initialize variables for form validation
+        $bookingTools = new Bookingtools();
+        //prep the data for saving in a new user object
+        $data['start_date'] = $start_date;
+        $data['property_id'] = $_SESSION['property_id'];
+        $member = unserialize($_SESSION['member_id']);
+        $data['booking_member_id'] = $member->member_id;
+        //create the new booking object
+        $newBooking = new Booking($data);
 
-    //save the new user to the database
-    $newBooking->save(true);
+        //save the new user to the database
+        $newBooking->save(true);
 
-    //redirect them to a welcome page
-    header("Location: propertyInfo.php");
+        //redirect them to a welcome page
+        header("Location: propertyInfo.php");
+    } else {
+        $message = "Invalid date!";
+    }
+   
 }
 
 if(isset($_POST['cancel-reg'])) { 
     header("Location: propertyInfo.php");
 } 
 
-//If the form wasn't submitted, or didn't validate
-//then we show the registration form again
 ?>
 <html>
 <head>
